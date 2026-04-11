724479c2f23e8e2a6825e29b127adbca6bc347e1
Serheyzoom <162877761+Serheyzoom@users.noreply.github.com>
2026-04-11 23:34:42 +0300

    Create mascots.js

---
+++ src/mascots.js
@@ -0,0 +1,7 @@
+export const mascots = {
+  happy: "https://github.com/Serheyzoom/shawarma-hero-miniapp/blob/main/радостного%20маскота.png?raw=true",
+  surprised: "https://github.com/Serheyzoom/shawarma-hero-miniapp/blob/main/Генерируй%20удивлённог.png?raw=true",
+  angry: "https://github.com/Serheyzoom/shawarma-hero-miniapp/blob/main/боевой%20маскот.png?raw=true",
+  super: "https://github.com/Serheyzoom/shawarma-hero-miniapp/blob/main/супер%E2%80%91героя.png?raw=true",
+  stamp: "https://github.com/Serheyzoom/shawarma-hero-miniapp/blob/main/тамп.png?raw=true"
+};
